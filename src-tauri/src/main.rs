3e8165ecae2a083321dab92502311a0838edf609
ctexthuang <ctexthuang@qq.com>
2026-05-14 17:41:04 +0800

    fix: hide Windows console in release builds

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,3 +1,6 @@
+// Prevents an additional console window on Windows in release builds.
+#![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
+
 fn main() {
     image_draw_ai_lib::run();
 }
